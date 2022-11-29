797cbfde5f0b63d45f020da69da695612aa1f22f
Camilo <jc_bernal2011@hotmail.com>
2022-11-28 21:54:00 -0500

    FXML

---
+++ src/control/AdminController.java
@@ -0,0 +1,54 @@
+package control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+
+import java.util.Optional;
+
+public class AdminController {
+
+    @FXML
+    private Button buttonA01;
+
+    @FXML
+    private Button buttonA02;
+
+    @FXML
+    private TextField textFieldNombreVisitante;
+
+    private Button button;
+
+    @FXML
+    void alert(ActionEvent event) {
+        Button b = (Button) event.getSource();
+        String apt = b.getText();
+        Alert alert;
+        if (!textFieldNombreVisitante.getText().isEmpty()){
+            alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Alerta de ingreso");
+            alert.setHeaderText("");
+            alert.setContentText("Se enviara una alerta de que"+textFieldNombreVisitante.getText()+"desea ir hacia el apartamento "+apt);
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK){
+                //mandar alerta de ingreso
+                if (apt.equals("A01")){
+                    //ENVIAR ALERTA A A01
+                } else {
+                    //ENVIAR ALERTA A A02
+                }
+                //Se borra el nombre
+                textFieldNombreVisitante.setText("");
+            }
+        } else {
+            alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("No se ha escrito ninguno nombre de visitante");
+            alert.setHeaderText("Visitante");
+            alert.setContentText("Recuerde que se debe informar el nombre de la persona que desea entrar");
+        }
+    }
+
+}
